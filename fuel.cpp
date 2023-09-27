84bfde81bf062e8fa66787f322091d3415d3d11a
tayyba678 <144408026+tayyba678@users.noreply.github.com>
2023-09-27 21:02:14 +0500

    Update fuel.cpp

--- fuel.cpp
+++ fuel.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
-void calculateFuel(float distance)
-{
-float fuel;
-fuel=distance*10;
-cout<<"Fuel needed: "<<fuel;
-}
+void calculateFuel(float distance);
 main(){
 cout<<"Enter the distance: ";
 float d;
 cin>>d;
 calculateFuel(d);
+}
+void calculateFuel(float distance)
+{
+float fuel;
+fuel=distance*10;
+cout<<"Fuel needed: "<<fuel;
 }

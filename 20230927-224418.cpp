c94a8b0d9647c7b92abb62dea5975acefc39146d
tayyba678 <144408026+tayyba678@users.noreply.github.com>
2023-09-27 22:43:37 +0500

    Add files via upload

---
+++ 20230927-224418.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void calculatePayableAmount(string day, float am);
+int main(){
+cout<<"Enter the day of purchase: ";
+string da;
+float amt;
+cin>> da;
+cout<<"Enter the total purchase amount: $";
+cin>>amt;
+calculatePayableAmount(da, amt);
+}
+void calculatePayableAmount(string day, float am)
+{
+if(day=="Sunday")
+{
+float amount,p;
+amount=am*0.1;
+p=am-amount;
+cout<<"Payable Amount: $"<<p;
+}
+if(day!="Sunday")
+{
+int amount;
+amount=am;
+cout<<"Payable Amount: $"<<amount;
+}
+}
